55e091fc5366a5d645f402262ed1f7abf1c9d0fa
robinozer <robin.ozer@outlook.com>
2023-01-20 09:45:17 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+QUESTIONS = {
+    "Aureolin is a shade of what color?": [
+        "Yellow", "Green", "Red", "Orange"
+    ],
+    "Cerulean is a shade of what color?": [
+        "Brown", "Blue", "Grey", "White"
+    ],
+    "Celadon is a shade of what color?": [
+        "White", "Black", "Grey", "Green"
+    ],
+    "Maroon is a shade of what color?": [
+        "Pink", "Blue", "Yellow", "Red"
+    ],
+    "Umber is a shade of what color?": [
+        "Yellow", "Red", "Brown", "Yellow"
+    ],
+}
+
+for question, options in QUESTIONS.items():
+    correct_option = options[0]
+    for option in options:
+        print(f" - {option}")
+
+    user_answer = input(f"{question}?")
+score = 0

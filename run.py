2fa842e67f6984a2142193b615626959079aa557
robinozer <robin.ozer@outlook.com>
2023-01-20 09:53:28 +0000

    add introduction with name input and welcome message

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+introduction = input("What is your name?\n")
+
+# Validate answer is filled in, and:
+
+print(f'Hello {introduction}, welcome to this Python Quiz. Each question has four answer options. You get one point for each correct answer.\nPress enter to start the quiz. Good luck {introduction}!')
 
 QUESTIONS = {
     "Aureolin is a shade of what color?": [
@@ -19,10 +24,16 @@ QUESTIONS = {
     ],
 }
 
+SCORE = 0
+
 for question, options in QUESTIONS.items():
     correct_option = options[0]
     for option in options:
         print(f" - {option}")
 
-    user_answer = input(f"{question}?")
-score = 0
+    user_answer = input(f"{question} ")
+    if user_answer == correct_option:
+        print(f"{user_answer} is correct. You're a genius!")
+        SCORE += 1
+    else:
+        print(f"{user_answer} is incorrect.\nThe correct answer is {correct_option}.")
